8360a3b3491e82f97501973737cc02cd1597b2a2
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2024-02-18 17:44:10 +0200

    added update route logics + POSTMAN verifying

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -103,7 +103,47 @@ const UserController = {
 		}
 	},
 	updateUser: async (req, res) => {
-		res.send('updateUser');
+		const { id } = req.params;
+		const { email, name, dateOfBirth, bio, location } = req.body;
+
+		let filePath;
+
+		if(req.file && req.file.path) {
+			filePath = req.file.path;
+		}
+
+		if(id !== req.user.userId) {
+			return res.status(403).json({ error: "Нет доступа" });
+		}
+
+		try {
+			if(email) {
+				const existingUser = await prisma.user.findFirst({
+					where: { email: email }
+				})
+
+				if(existingUser && existingUser.id !== id) {
+					return res.status(400).json({ error: "Почта уже используется" })
+				}
+			}
+
+			const user = await prisma.user.update({
+				where: { id },
+				data: {
+					email: email || undefined,
+					name: name || undefined,
+					avatarUrl: filePath ? `/${filePath}` : undefined,
+					dateOfBirth: dateOfBirth || undefined,
+					bio: bio || undefined,
+					location: location || undefined
+				}
+			})
+
+			res.json(user);
+		} catch(error) {
+			console.log('Update error', error);
+			res.status(500).json({ error: "Internal server error" });
+		}
 	},
 	current: async (req, res) => {
 		res.send('current');
