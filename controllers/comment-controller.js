73cb498898cb4589a21d5b849883f071d78cccc1
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2024-02-18 22:14:49 +0200

    added delete comment func in CommentController

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -25,8 +25,31 @@ const CommentController = {
 		}
 	}, 
 	deleteComment: async (req, res) => {
-		res.send("deleteComment");
-	}
+    try {
+      const { id } = req.params;
+      const userId = req.user.userId;
+
+      // Check if comment exists
+      const comment = await prisma.comment.findUnique({ where: { id } });
+
+      if (!comment) {
+        return res.status(404).json({ error: 'Комментарий не найден' });
+      }
+
+      // Check if the user is the owner of the comment
+      if (comment.userId !== userId) {
+        return res.status(403).json({ error: 'Вы не авторизованы для удаления этого комментария' });
+      }
+
+      // Delete the comment
+      await prisma.comment.delete({ where: { id } });
+
+      res.json(comment);
+    } catch (error) {
+      console.error('Error deleting comment:', error);
+      res.status(500).json({ error: 'Не удалось удалить комментарий' });
+    }
+  }
 }
 
 module.exports = CommentController;
