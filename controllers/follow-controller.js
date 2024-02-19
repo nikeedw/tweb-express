b5ffb6b99276df1b4a4147938c080964cfde72c9
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2024-02-19 19:09:41 +0200

    created unfollow method

--- controllers/follow-controller.js
+++ controllers/follow-controller.js
@@ -32,12 +32,37 @@ const FollowController = {
 
 			res.status(201).json({ message: 'Подписка успешно создана' });
 		} catch (error) {
-			console.log('error', error)
-			res.status(500).json({ error: 'Ошибка сервера' });
+			console.log("Follow user error", error)
+			res.status(500).json({ error: "Internal server error" });
 		}
 	},
 	unfollowUser: async (req, res) => {
-		res.send("Unfollow User");
+		const { followingId } = req.body;
+		const userId = req.user.userId;
+
+		try {
+			const follows = await prisma.follows.findFirst({
+				where: {
+					AND: [
+						{followerId: userId},
+						{followingId}
+					]
+				}
+			})
+
+			if(!follows) {
+				return res.status(404).json({ error: "Вы не подписаны на этого пользователя" });
+			}
+
+			await prisma.follows.delete({
+				where: {id: follows.id}
+			})
+
+			res.status(201).json({ error: "Вы отписались" })
+		} catch(error) {
+			console.log("Unfollow user error", error)
+			res.status(500).json({ error: "Internal server error" });
+		}
 	}
 }
 
